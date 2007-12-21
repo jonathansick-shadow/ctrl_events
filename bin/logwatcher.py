f4794d671defe1c9e6e009d6c84fa9cfbaa91292
srp <srp@git.lsstcorp.org>
2007-12-21 16:52:29 +0000

    Added command line argument to specify the host

--- bin/logwatcher.py
+++ bin/logwatcher.py
@@ -2,10 +2,12 @@
 import lsst.mwi.data as datap
 import lsst.events as events
 import time
+import sys
 
 if __name__ == "__main__":
+    host = sys.argv[1]
     print "starting...\n"
-    x = events.EventReceiver("lsst8.ncsa.uiuc.edu", "LSSTLogging")
+    x = events.EventReceiver(host, "LSSTLogging")
     while (True): 
         print "waiting on logging event...\n"
         val = x.receive()
