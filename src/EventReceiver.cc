4e3e20c376213840e744a373a0a98063e35ae587
srp <srp@git.lsstcorp.org>
2009-04-17 17:58:17 +0000

    handle daf::base::DateTime properly

--- src/EventReceiver.cc
+++ src/EventReceiver.cc
@@ -14,6 +14,7 @@
 #include <cstring>
 
 #include "lsst/ctrl/events/EventReceiver.h"
+#include "lsst/daf/base/DateTime.h"
 #include "lsst/daf/base/PropertySet.h"
 #include "lsst/pex/exceptions.h"
 #include "lsst/pex/logging/Component.h"
@@ -470,10 +471,13 @@ PropertySet::Ptr EventReceiver::unmarshall(const std::string& text) {
             value = boost::any(double_value);
             psp->add(key, double_value);
         } else if (type == "string") {
-            std::string string_value;
-            iss >> string_value;
-            psp->add(key, string_value);
+            psp->add(key, val);
+        } else if (type == "datetime") {
+            long long longlong_value;
+            iss >> longlong_value;
+            psp->add(key, lsst::daf::base::DateTime(longlong_value, lsst::daf::base::DateTime::UTC));
         }
+
         // type == nodelist can be ignored
     }
         
