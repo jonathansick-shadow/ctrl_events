e92e4e3513ec4a6f59ed1ef6dbc704a3adbed060
Stephen R. Pietrowicz <srp@ncsa.illinois.edu>
2015-02-18 13:29:57 -0600

    Adding EventAppender code and unit test.

---
+++ tests/EventAppenderTest.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+# 
+# LSST Data Management System
+#
+# Copyright 2008-2014  AURA/LSST.
+# 
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the LSST License Statement and 
+# the GNU General Public License along with this program.  If not, 
+# see <https://www.lsstcorp.org/LegalNotices/>.
+#
+
+"""
+This tests the logging system in a variety of ways.
+"""
+
+import lsst.log as log
+import os
+import shutil
+import sys
+import tempfile
+import unittest
+import lsst.ctrl.events
+
+class TestLog(unittest.TestCase):
+
+    class StdoutCapture(object):
+        """
+        Context manager to redirect stdout to a file.
+        """
+
+        def __init__(self, filename):
+            self.stdout = None
+            self.outputFilename = filename
+
+        def __enter__(self):
+            self.stdout = os.dup(1)
+            os.close(1)
+            os.open(self.outputFilename, os.O_WRONLY | os.O_CREAT | os.O_TRUNC)
+
+        def __exit__(self, type, value, traceback):
+            if self.stdout is not None:
+                os.close(1)
+                os.dup(self.stdout)
+                os.close(self.stdout)
+                self.stdout = None
+
+    def setUp(self):
+        """Make a temporary directory and a log file in it."""
+        self.tempDir = tempfile.mkdtemp()
+        self.outputFilename = os.path.join(self.tempDir, "log.out")
+        self.stdout = None
+
+    def tearDown(self):
+        """Remove the temporary directory."""
+        shutil.rmtree(self.tempDir)
+
+    def configure(self, configuration):
+        """
+        Create a configuration file in the temporary directory and populate
+        it with the provided string.
+        """
+        log.configure_prop(configuration.format(self.outputFilename))
+
+    def check(self, reference):
+        """Compare the log file with the provided reference text."""
+        with open(self.outputFilename, 'r') as f:
+            lines = [l.split(']')[-1] for l in f.readlines()]
+            reflines = [l + "\n" for l in reference.split("\n") if l != ""]
+            map(self.assertEqual, lines, reflines)
+
+
+###############################################################################
+
+    def testFileAppender(self):
+        """Test configuring logging to go to a file."""
+        self.configure("""
+log4j.rootLogger=TRACE, EA
+log4j.appender.EA=EventAppender
+""")
+        log.MDC("x", 3)
+        with log.LogContext("component"):
+            log.trace("This is TRACE")
+            log.info("This is INFO")
+            log.debug("This is DEBUG")
+        log.MDCRemove("x")
+
+####################################################################################
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
